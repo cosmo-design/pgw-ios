63b30c6338db47fe4eaf3e2423c85d8831b2da78
John George <ionicrainfall@pm.me>
2026-08-25 23:21:18 -0500

    Initial Commit

---
+++ Nexal PortalTests/Nexal_PortalTests.swift
@@ -0,0 +1,19 @@
+//
+//  Nexal_PortalTests.swift
+//  Nexal PortalTests
+//
+//  Created by John George on 8/25/26.
+//
+
+import Testing
+@testable import Nexal_Portal
+
+struct Nexal_PortalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
